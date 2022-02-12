b22cc778b1cf2a99b1e4a21a9f75318ed4bc22be
Caleb Mensa-Bonsu <calebmensabonsu@gmail.com>
2022-02-12 20:16:11 +0000

    Added routes to my_app file

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         '/home':(context) => const Homepage(),
         '/secondPage':(context) => const SecondPage(),
 
+
       },
 
       theme: ThemeData(
